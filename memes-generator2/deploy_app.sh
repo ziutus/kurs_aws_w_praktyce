6864ab6ebf800ec95ad48802afab9338d634a3c6
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2021-07-18 21:37:48 +0200

    ref: PROJECT and STAGE are now variables to easier manage

--- memes-generator2/deploy_app.sh
+++ memes-generator2/deploy_app.sh
@@ -2,8 +2,11 @@
 set -u
 set -e 
 
-# action='create_paid'
-action='delete_paid'
+PROJECT="memes-generator2"
+STAGE="dev"
+
+action='create_paid'
+# action='delete_paid'
 
 STEP=12
 
@@ -14,30 +17,32 @@ STEP=12
 if [ $action == "delete_paid" ]; then
     set +x
     echo "Stopping db instance - it can take 2-3 minutes"
-    aws rds stop-db-instance --db-instance-identifier memes-generator2-dev-data-db
-    aws cloudformation delete-stack --stack memes-generator2-dev-load-balancing
-    aws cloudformation delete-stack --stack memes-generator2-dev-application-auto-scaling
-    aws cloudformation delete-stack --stack memes-generator2-dev-application-application-instance
-    aws cloudformation delete-stack --stack memes-generator2-dev-operations-jumphost-db
-    aws cloudformation delete-stack --stack memes-generator2-dev-network-nat-gateway-azA
-    aws cloudformation delete-stack --stack memes-generator2-dev-network-nat-gateway-azB
+    aws rds stop-db-instance --db-instance-identifier ${PROJECT}-${STAGE}-data-db
+    aws cloudformation delete-stack --stack ${PROJECT}-${STAGE}-load-balancing
+    aws cloudformation delete-stack --stack ${PROJECT}-${STAGE}-application-auto-scaling
+    aws cloudformation delete-stack --stack ${PROJECT}-${STAGE}-application-application-instance
+    aws cloudformation delete-stack --stack ${PROJECT}-${STAGE}-operations-jumphost-db
+    aws cloudformation delete-stack --stack ${PROJECT}-${STAGE}-network-nat-gateway-azA
+    aws cloudformation delete-stack --stack ${PROJECT}-${STAGE}-network-nat-gateway-azB
     set -x
 fi
 
 if [ $action == "create_paid" ]; then
     echo "Starting db instance - it can take 2-3 minutes"
-    aws rds start-db-instance --db-instance-identifier memes-generator2-dev-data-db
-    ./deploy.sh --project memes-generator2 --stage dev --component network --stack nat-gateway-azA --exec
-    ./deploy.sh --project memes-generator2 --stage dev --component network --stack nat-gateway-azB --exec
-    # ./deploy.sh --project memes-generator2 --stage dev --component application --stack application-instance --exec
-    ./deploy.sh --project memes-generator2 --stage dev --component operations --stack jumphost-db --exec
-    ./deploy.sh --project memes-generator2 --stage dev --component network --stack application-auto-scaling --exec
-    ./deploy.sh --project memes-generator2 --stage dev --component network --stack load-balancing --exec
+    # TODO add check if db is already started
+    # aws rds start-db-instance --db-instance-identifier ${PROJECT}-${STAGE}-data-db
+    ./deploy.sh --project ${PROJECT} --stage $STAGE --component network --stack nat-gateway-azA --exec
+    ./deploy.sh --project ${PROJECT} --stage $STAGE --component network --stack nat-gateway-azB --exec
+    # ./deploy.sh --project ${PROJECT} --stage $STAGE --component application --stack application-instance --exec
+    ./deploy.sh --project ${PROJECT} --stage $STAGE --component operations --stack jumphost-db --exec
+    ./deploy.sh --project ${PROJECT} --stage $STAGE --component network --stack application-auto-scaling --exec
+    ./deploy.sh --project ${PROJECT} --stage $STAGE --component network --stack load-balancing --exec
+
 fi
 
 
 ### week 9
-# [ $STEP -eq 11 ] && ./deploy.sh --project memes-generator2 --stage dev --component application --stack bucket --params website --region "us-east-1" --exec
+# [ $STEP -eq 11 ] && ./deploy.sh --project ${PROJECT} --stage dev --component application --stack bucket --params website --region "us-east-1" --exec
 
 
 exit 0
