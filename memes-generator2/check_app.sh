719e3c2798c2c56a254c9457f89c060064380cec
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2021-07-19 21:00:07 +0200

    add ALB check script

---
+++ memes-generator2/check_app.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+set -u
+
+# curl -X GET http://localhost:8080/actuator/health
+
+REGION=$(echo $AWS_REGION)
+
+
+ALB_URL=$(aws ssm get-parameter --name '/memes-generator2/dev/network/alb/url' --query 'Parameter.Value' --region $REGION 2>&1 )
+RC=$?
+
+if [ $RC -ne 0 ]; then
+    echo "The Application load balancer is not configured"
+    exit 1
+fi
+
+ALB_URL=$(echo $ALB_URL | sed s/\"//g)
+echo "ALB_URL: $ALB_URL"
+
+STATUS=$(curl -s -X GET http://${ALB_URL}/actuator/health | jq -r '.status')
+
+if [ $STATUS == 'UP' ]; then
+    echo "ALB status: [OK]"
+else 
+    echo "Something went wrong, status is: >$STATUS<"
+    exit 1    
+fi
+
+exit 0
