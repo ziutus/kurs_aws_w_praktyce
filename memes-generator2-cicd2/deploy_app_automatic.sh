285070178c8d1ddb9d0894768aea62e05abf977c
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2021-07-26 13:06:04 +0200

    feat: step 7 - document with command to create DB user

--- memes-generator2-cicd2/deploy_app_automatic.sh
+++ memes-generator2-cicd2/deploy_app_automatic.sh
@@ -3,13 +3,15 @@ export PROJECT="memes-generator2-cicd2"
 export SHARED_STAGE="shared-dev"
 export STAGE=""
 
-STEP=5
+STEP=7
 
 [[ $STEP -le 1 ]] && ./deploy.sh --component cicd --stack account-parameters --exec
 [[ $STEP -le 2 ]] && ./deploy.sh --component cicd --stack git-repository --params cicd --exec
 [[ $STEP -le 3 ]] && ./deploy.sh --component cicd --stack git-repository --params infra --exec
 [[ $STEP -le 4 ]] && ./deploy.sh --component cicd --stack kms --exec
 [[ $STEP -le 5 ]] && ./deploy.sh --component cicd --stack cicd-roles --exec
+# step 6 connected with step 6
+[[ $STEP -le 7 ]] && ./deploy.sh --component cicd --stack ssm-command-create-db-user --exec
 
 
 exit 0
