10a936d813b1eac5949236ea560e44f860dcbc9c
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2021-07-26 16:30:55 +0200

    feat: step 8: cicd buckets and roles

--- memes-generator2-cicd2/deploy_app_automatic.sh
+++ memes-generator2-cicd2/deploy_app_automatic.sh
@@ -3,7 +3,7 @@ export PROJECT="memes-generator2-cicd2"
 export SHARED_STAGE="shared-dev"
 export STAGE=""
 
-STEP=7
+STEP=8
 
 [[ $STEP -le 1 ]] && ./deploy.sh --component cicd --stack account-parameters --exec
 [[ $STEP -le 2 ]] && ./deploy.sh --component cicd --stack git-repository --params cicd --exec
@@ -12,6 +12,7 @@ STEP=7
 [[ $STEP -le 5 ]] && ./deploy.sh --component cicd --stack cicd-roles --exec
 # step 6 connected with step 6
 [[ $STEP -le 7 ]] && ./deploy.sh --component cicd --stack ssm-command-create-db-user --exec
+[[ $STEP -le 8 ]] && ./deploy.sh --component cicd --stack cicd-resources --exec
 
 
 exit 0
