65ea194950cb782cf8b7f51bfd5c569274013ef1
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2021-08-10 05:32:53 +0200

    feat: test and build project automatically

--- memes-generator2-cicd2/deploy_app_automatic.sh
+++ memes-generator2-cicd2/deploy_app_automatic.sh
@@ -5,14 +5,19 @@ export STAGE=""
 
 STEP=8
 
-[[ $STEP -le 1 ]] && ./deploy.sh --component cicd --stack account-parameters --exec
-[[ $STEP -le 2 ]] && ./deploy.sh --component cicd --stack git-repository --params cicd --exec
-[[ $STEP -le 3 ]] && ./deploy.sh --component cicd --stack git-repository --params infra --exec
-[[ $STEP -le 4 ]] && ./deploy.sh --component cicd --stack kms --exec
-[[ $STEP -le 5 ]] && ./deploy.sh --component cicd --stack cicd-roles --exec
+[[ $STEP -le 1 ]] && ./deploy.sh --component cicd --template-name account-parameters --exec
+[[ $STEP -le 2 ]] && ./deploy.sh --component cicd --template-name git-repository --params cicd --exec
+[[ $STEP -le 3 ]] && ./deploy.sh --component cicd --template-name git-repository --params infra --exec
+[[ $STEP -le 4 ]] && ./deploy.sh --component cicd --template-name kms --exec
+[[ $STEP -le 5 ]] && ./deploy.sh --component cicd --template-name cicd-roles --exec
 # step 6 connected with step 6
-[[ $STEP -le 7 ]] && ./deploy.sh --component cicd --stack ssm-command-create-db-user --exec
-[[ $STEP -le 8 ]] && ./deploy.sh --component cicd --stack cicd-resources --exec
+[[ $STEP -le 7 ]] && ./deploy.sh --component cicd --template-name ssm-command-create-db-user --exec
+[[ $STEP -le 8 ]] && ./deploy.sh --component cicd --template-name cicd-resources --exec
+# [[ $STEP -le 9 ]] && ./deploy.sh --component cicd --template-name pipeline-resources-create --exec
+
+#  creating code build projects to run shell scripts
+# [[ $STEP -le 10 ]] && ./deploy.sh --component cicd --template-name build-project --params create-db-user --exec
+# [[ $STEP -le 11 ]] && ./deploy.sh --component cicd --template-name build-project --params test-and-create-config --exec
 
 
 exit 0
